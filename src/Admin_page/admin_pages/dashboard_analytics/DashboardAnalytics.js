4c78fac68f4894acfd1c88c9b9cb762b217572f9
Gloriazira <gloriazira9@gmail.com>
2023-10-04 22:28:21 +0100

    added the dynamic barchart on the dashboard analytic page

--- src/Admin_page/admin_pages/dashboard_analytics/DashboardAnalytics.js
+++ src/Admin_page/admin_pages/dashboard_analytics/DashboardAnalytics.js
@@ -4,11 +4,13 @@ import { useNavigate } from "react-router-dom";
 import admin from "../../../class/admin.class";
 import { useQuery } from 'react-query';
 import { toast } from "react-toastify";
-import StackedBarChart from "./DashboardAnalyticBarchart";
 import './dasboardanalytics.css';
 import DonutPieChart from "./DashboardPieChart";
 import dataOBJs from "../../../class/data.class";
 import AdminEmployeeFilterComponent from "../admin_employee_list_page/AdminEmployeeFilterComponent";
+import ChartComponent from "./DashboardAnalyticBarchart";
+
+
 
 
 
@@ -45,28 +47,9 @@ const fetchWardsData = async (key) => {
 
 
 
-export default function DashboardAnalytics({ selectedTypology }) {
-
+export default function DashboardAnalytics() {
     const { data, status } = useQuery(['fetchDataSummary'], fetchDataSummary);
-    const { data: wardsData, status: wardStatus } = useQuery(['fetchWardsData'], fetchWardsData);
 
-
-
-
-    const localGovernmentData = [
-
-        {
-            name: 'Fufore',
-            males: 500,
-            females: 250,
-        },
-        {
-            name: 'Guyuk',
-            males: 400,
-            females: 600,
-        },
-
-    ];
 
     const maleCount = 250;
     const femaleCount = 350;
@@ -74,13 +57,11 @@ export default function DashboardAnalytics({ selectedTypology }) {
     const nonsocuCount = 50;
 
 
-
     return (
         <div className="dashboard-analytic-section">
 
             <div>
                 <div className="d-flex align-items-center justify-content-between summary-card">
-
                     <div className="card">
                         <h1 className="number">{data?.data?.beneficiaryCount}</h1>
                         <p>Total Beneficiaries</p>
@@ -89,7 +70,6 @@ export default function DashboardAnalytics({ selectedTypology }) {
                         <h1 className="number">{data?.data?.lgaCount}</h1>
                         <p>Total LGA's</p>
                     </div>
-
                     <div className="card">
                         <h1 className="number">2</h1>
                         <p>Total Wards</p>
@@ -97,28 +77,22 @@ export default function DashboardAnalytics({ selectedTypology }) {
                 </div>
             </div>
 
-            <div className="dashboard-analytic-graps mt-5">
-                <div className="container-fluid p-3">
+            <div className=" d-flex justify-content-around dashboard-analytic-graps mt-5 ">
+                <div className="container-fluid p-3 mt-3">
                     <div className="d-flex align-items-center justify-content-between filter-dashboard-analytic">
                         <h1 className="">{data?.data?.beneficiaryCount}</h1>
-                        <AdminEmployeeFilterComponent showLastSelect={false}  />
+                        <AdminEmployeeFilterComponent showLastSelect={false} />
                     </div>
                     <div className="dashboard-analytic-barchart mt-5">
-
-
-                        <StackedBarChart data={localGovernmentData} />
-
-
+                       <ChartComponent />
                     </div>
                 </div>
 
 
-                <div className="d-flex align-items-center justify-content-around dashboard-analytic-pie mt-4 p-3" >
+                <div className=" dashboard-analytic-pie px-3" >
 
                     <div className="d-flex flex-column align-items-center donut-pie-chart-div">
                         <p>Gender Representation</p>
-                        {/* <DonutPieChart maleCount={maleCount} femaleCount={femaleCount} /> */}
-
                         <DonutPieChart data={[
                             { label: 'Male', value: maleCount },
                             { label: 'Female', value: femaleCount },
